8430c51bdf1eb20954da773d3e32f50644cbf651
Will Grey <w.m.f.grey@gmail.com>
2015-04-08 10:47:35 +0100

    Name change and corrected input error

--- writeReports.sh
+++ writeReports.sh
@@ -1,25 +1,26 @@
 #! /bin/bash -f
 
-# gcc commentsBank.c -o commentsBank -Wall -lm
+ gcc reportGenerator.c -o reportGenerator -Wall -lm
+
+if [ 0 ] ; then 
+
+ [ -f files/reports.txt ] && rm -f files/reports.txt
+ [ -f files/classListOut.txt ] && rm -f files/classListOut.txt
+
+ ./reportGenerator -grades files/classList.txt files/classListOut.txt 12 < files/intest.txt
+  gedit files/classListOut.txt
+ #./reportGenerator -reports files/classListOut.txt files/bank.txt files/reports.txt 12
+ #gedit files/reports.txt
+
+fi
 
 if [ 1 ] ; then 
 
  [ -f files/reports.txt ] && rm -f files/reports.txt
  [ -f files/classListOut.txt ] && rm -f files/classListOut.txt
 
- ./commentsBank files/classList.txt files/classListOut.txt 12 < files/intest.txt
- #./commentsBank -reports files/classListOut.txt files/bank.txt files/reports.txt 12
- #gedit files/reports.txt
-
-fi
-
-if [ ] ; then 
-
- [ -f files/reports.txt ] && rm -f files/reports.txt
- [ -f files/classListOut.txt ] && rm -f files/classListOut.txt
-
- ./commentsBank -grades files/classList.txt files/classListOut.txt 12 < files/intest.txt
- ./commentsBank -reports files/classListOut.txt files/bank.txt files/reports.txt 12
+ ./reportGenerator -grades files/classList.txt files/classListOut.txt 12 < files/intest.txt
+ ./reportGenerator -reports files/classListOut.txt files/bank.txt files/reports.txt 12
  gedit files/reports.txt
 
 fi
@@ -30,8 +31,8 @@ if [ ] ; then
  [ -f files/reports.txt ] && rm -f files/reports.txt
  [ -f files/classListOut.txt ] && rm -f files/classListOut.txt
 
- ./commentsBank -grades files/classList.txt files/classListOut.txt 7 < files/intest.txt
- ./commentsBank -reports files/classListOut.txt files/bank.txt files/reports.txt 7
+ ./reportGenerator -grades files/classList.txt files/classListOut.txt 7 < files/intest.txt
+ ./reportGenerator -reports files/classListOut.txt files/bank.txt files/reports.txt 7
  gedit files/reports.txt
 
 fi
